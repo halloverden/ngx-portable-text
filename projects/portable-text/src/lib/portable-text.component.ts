0466521bf9124a6c0f94fa17b9f7dd7c6bbc9d45
Andreas Larssen <andreas@halloverden.no>
2022-04-11 19:14:07 +0200

    EDIT: simple portable text mock renders correctly

--- projects/portable-text/src/lib/portable-text.component.ts
+++ projects/portable-text/src/lib/portable-text.component.ts
@@ -11,6 +11,7 @@ import {
 import {PortableTextInterface} from './interfaces/portable-text.interface';
 import {PortableTextConfigInterface} from './interfaces/portable-text-config.interface';
 import {ChildInterface} from './interfaces/child.interface';
+import {MarkDefInterface} from './interfaces/mark-def.interface';
 
 @Component({
   selector: 'ngx-portable-text',
@@ -67,6 +68,99 @@ export class PortableTextComponent implements OnInit, OnChanges {
   }
 
   /**
+   *
+   * @param mark
+   * @param markedChild
+   * @private
+   */
+  private addMark(mark: string, markedChild: string): string {
+    // TODO: Define html (string/function) somewhere and use it here
+    switch (mark) {
+      case 'strong':
+        return '<strong>' + markedChild + '</strong>';
+      default:
+        throw Error('Unknown type'); // TODO: Handle unknown types
+    }
+  }
+
+  /**
+   *
+   * @param markDef
+   * @param markedChild
+   * @private
+   */
+  private addMarkDef(markDef: MarkDefInterface, markedChild: string): string {
+    // TODO: Define html (string/function) somewhere and use it here
+    switch (markDef._type) {
+      case 'link':
+        return '<a href="' + markDef.href + '">' + markedChild + '</a>';
+      default:
+        throw Error('Unknown type'); // TODO: Handle unknown types
+    }
+  }
+
+  /**
+   *
+   * @param portableText
+   * @param child
+   * @private
+   */
+  private addMarks(portableText: PortableTextInterface, child: ChildInterface): string {
+    let markedChild = child.text;
+
+    // TODO: Order? Link (and other types?) should be the outer element
+    child.marks.forEach((mark: string) => {
+      const markDef = this.getMarkDef(portableText, mark);
+      if (!markDef) {
+        markedChild = this.addMark(mark, markedChild);
+      } else {
+        markedChild = this.addMarkDef(markDef, markedChild);
+      }
+    });
+
+    return markedChild;
+  }
+
+  /**
+   *
+   * @param portableText
+   * @param mark
+   * @private
+   */
+  private getMarkDef(portableText: PortableTextInterface, mark: string): MarkDefInterface|null {
+    let markDef = null;
+
+    portableText.markDefs?.forEach((targetMarkDef: MarkDefInterface) => {
+      if (targetMarkDef._key === mark) {
+        markDef = targetMarkDef;
+      }
+    });
+
+    return markDef;
+  }
+
+  /**
+   *
+   * @param portableText
+   * @private
+   */
+  private renderBlock(portableText: PortableTextInterface): void {
+    const pTag = this.renderer.createElement('p');
+    let blockContent = '';
+
+    portableText.children?.forEach((child: ChildInterface) => {
+      if (child.marks.length !== 0) {
+        blockContent += this.addMarks(portableText, child);
+      } else {
+        blockContent += child.text;
+      }
+    });
+
+    pTag.innerHTML = blockContent;
+    this.renderer.appendChild(this.element.nativeElement, pTag);
+  }
+
+  /**
    * @private
    */
   private renderContent(): void {
@@ -74,24 +168,12 @@ export class PortableTextComponent implements OnInit, OnChanges {
     // console.log(this.config);
 
     this.portableTexts.forEach((portableText: PortableTextInterface) => {
-      console.log('TEXT: ', portableText);
-
       switch (portableText._type) {
         case 'block':
-          const pTag = this.renderer.createElement('p');
-          let text = '';
-
-          portableText.children?.forEach((child: ChildInterface) => {
-            console.log('CHILD: ', child);
-            text += child.text;
-          });
-
-          const textContent = this.renderer.createText(text);
-          this.renderer.appendChild(pTag, textContent);
-          this.renderer.appendChild(this.element.nativeElement, pTag);
+          this.renderBlock(portableText);
           break;
         default:
-          throw Error('Unknown type'); // TODO: Handle other types
+          throw Error('Unknown type'); // TODO: Handle unknown types
       }
     });
   }
