172dbaa4d48708340894e292eaf4071005abb028
Andreas Larssen <andreas@halloverden.no>
2022-04-19 18:17:18 +0200

    EDIT: move rendering logic to service

--- projects/portable-text/src/lib/components/p-tag/p-tag.component.ts
+++ projects/portable-text/src/lib/components/p-tag/p-tag.component.ts
@@ -1,7 +1,5 @@
 import {ChangeDetectionStrategy, Component, ElementRef, Input, OnInit} from '@angular/core';
 import {PortableTextInterface} from '../../interfaces/portable-text.interface';
-import {ChildInterface} from '../../interfaces/child.interface';
-import {MarkDefInterface} from '../../interfaces/mark-def.interface';
 import {PortableTextConfigInterface} from '../../interfaces/portable-text-config.interface';
 import {RenderService} from '../../services/render.service';
 
@@ -29,96 +27,6 @@ export class PTagComponent implements OnInit {
    *
    */
   ngOnInit(): void {
-    this.renderContent();
-  }
-
-  /**
-   *
-   * @param mark
-   * @param markedChild
-   * @private
-   */
-  private addMark(mark: string, markedChild: string): string {
-    // TODO: Define html (string/function) somewhere and use it here
-    switch (mark) {
-      case 'strong':
-        return '<strong>' + markedChild + '</strong>';
-      default:
-        throw Error('Unknown mark type ' + mark); // TODO: Handle unknown types
-    }
-  }
-
-  /**
-   *
-   * @param markDef
-   * @param markedChild
-   * @private
-   */
-  private addMarkDef(markDef: MarkDefInterface, markedChild: string): string {
-    // TODO: Define html (string/function) somewhere and use it here
-    switch (markDef._type) {
-      case 'link':
-        return '<a href="' + markDef.href + '">' + markedChild + '</a>';
-      default:
-        throw Error('Unknown markDef type: ' + markDef._type); // TODO: Handle unknown types
-    }
-  }
-
-  /**
-   *
-   * @param portableText
-   * @param child
-   * @private
-   */
-  private addMarks(portableText: PortableTextInterface, child: ChildInterface): string {
-    let markedChild = child.text;
-
-    // TODO: Order? Link (and other types?) should be the outer element
-    child.marks.forEach((mark: string) => {
-      const markDef = this.getMarkDef(portableText, mark);
-      if (!markDef) {
-        markedChild = this.addMark(mark, markedChild);
-      } else {
-        markedChild = this.addMarkDef(markDef, markedChild);
-      }
-    });
-
-    return markedChild;
-  }
-
-  /**
-   *
-   * @param portableText
-   * @param mark
-   * @private
-   */
-  private getMarkDef(portableText: PortableTextInterface, mark: string): MarkDefInterface|null {
-    let markDef = null;
-
-    portableText.markDefs?.forEach((targetMarkDef: MarkDefInterface) => {
-      if (targetMarkDef._key === mark) {
-        markDef = targetMarkDef;
-      }
-    });
-
-    return markDef;
-  }
-
-  /**
-   * @private
-   */
-  private renderContent(): void {
-    // TODO: Move blockContent to this.renderService and reuse in all content comps
-    let blockContent = '';
-
-    this.portableText.children?.forEach((child: ChildInterface) => {
-      if (child.marks.length !== 0) {
-        blockContent += this.addMarks(this.portableText, child);
-      } else {
-        blockContent += child.text;
-      }
-    });
-
-    this.element.nativeElement.innerHTML = blockContent;
+    this.element.nativeElement.innerHTML = this.renderService.renderContent(this.portableText);
   }
 }
--- projects/portable-text/src/lib/services/render.service.ts
+++ projects/portable-text/src/lib/services/render.service.ts
@@ -1,8 +1,100 @@
 import {Injectable} from '@angular/core';
+import {MarkDefInterface} from '../interfaces/mark-def.interface';
+import {PortableTextInterface} from '../interfaces/portable-text.interface';
+import {ChildInterface} from '../interfaces/child.interface';
 
 @Injectable({
   providedIn: 'root'
 })
 export class RenderService {
+  /**
+   * TODO: Rename method?
+   *
+   * @param portableText
+   */
+  renderContent(portableText: PortableTextInterface): string {
+    let blockContent = '';
 
+    portableText.children?.forEach((child: ChildInterface) => {
+      if (child.marks.length !== 0) {
+        blockContent += this.addMarks(portableText, child);
+      } else {
+        blockContent += child.text;
+      }
+    });
+
+    return blockContent;
+  }
+
+  /**
+   *
+   * @param portableText
+   * @param child
+   * @private
+   */
+  private addMarks(portableText: PortableTextInterface, child: ChildInterface): string {
+    let markedChild = child.text;
+
+    // TODO: Order? Link (and other types?) should be the outer element
+    child.marks.forEach((mark: string) => {
+      const markDef = this.getMarkDef(portableText, mark);
+      if (!markDef) {
+        markedChild = RenderService.addMark(mark, markedChild);
+      } else {
+        markedChild = RenderService.addMarkDef(markDef, markedChild);
+      }
+    });
+
+    return markedChild;
+  }
+
+  /**
+   *
+   * @param portableText
+   * @param mark
+   * @private
+   */
+  private getMarkDef(portableText: PortableTextInterface, mark: string): MarkDefInterface|null {
+    let markDef = null;
+
+    portableText.markDefs?.forEach((targetMarkDef: MarkDefInterface) => {
+      if (targetMarkDef._key === mark) {
+        markDef = targetMarkDef;
+      }
+    });
+
+    return markDef;
+  }
+
+  /**
+   *
+   * @param mark
+   * @param markedChild
+   * @private
+   */
+  private static addMark(mark: string, markedChild: string): string {
+    // TODO: Define html (string/function) somewhere and use it here
+    switch (mark) {
+      case 'strong':
+        return '<strong>' + markedChild + '</strong>';
+      default:
+        throw Error('Unknown mark type ' + mark); // TODO: Handle unknown types
+    }
+  }
+
+  /**
+   *
+   * @param markDef
+   * @param markedChild
+   * @private
+   */
+  private static addMarkDef(markDef: MarkDefInterface, markedChild: string): string {
+    // TODO: Define html (string/function) somewhere and use it here
+    switch (markDef._type) {
+      case 'link':
+        return '<a href="' + markDef.href + '">' + markedChild + '</a>';
+      default:
+        throw Error('Unknown markDef type: ' + markDef._type); // TODO: Handle unknown types
+    }
+  }
 }
