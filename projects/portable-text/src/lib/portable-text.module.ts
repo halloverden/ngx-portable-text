04b39877e96711bfda75cca99bed065305933be8
Andreas Larssen <andreas@halloverden.no>
2022-04-24 23:30:44 +0200

    ADD: ul support

--- projects/portable-text/src/lib/portable-text.module.ts
+++ projects/portable-text/src/lib/portable-text.module.ts
@@ -3,12 +3,14 @@ import { PortableTextComponent } from './portable-text.component';
 import {CommonModule} from '@angular/common';
 import {PTagComponent} from './components/p-tag/p-tag.component';
 import {RenderCustomComponentComponent} from './components/render-custom-component/render-custom-component.component';
+import {UlTagComponent} from "./components/ul-tag/ul-tag.component";
 
 @NgModule({
   declarations: [
     PortableTextComponent,
     PTagComponent,
-    RenderCustomComponentComponent
+    RenderCustomComponentComponent,
+    UlTagComponent
   ],
   imports: [
     CommonModule,
