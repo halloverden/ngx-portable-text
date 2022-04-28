452f3f43db8bbcdcc65737314c9a35a6b4b956da
Erich Json Fosse <erich@forbundspartner.no>
2022-04-28 18:29:20 +0200

    ADD: Paragraph renderer now uses marker provider

--- projects/portable-text/src/lib/html-renderers/paragraph.renderer.ts
+++ projects/portable-text/src/lib/html-renderers/paragraph.renderer.ts
@@ -4,19 +4,61 @@ import {
   ClassifiedArbitraryTypedObjectType
 } from '../helpers/arbitrary-typed-object.helper';
 import { Injectable } from '@angular/core';
+import { MarkerProviderService } from '../services/marker-provider.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ParagraphRenderer implements HtmlRendererInterface {
+  constructor(private markerProvider: MarkerProviderService) {}
+
   /**
    *
    * @param cato
    */
   render(cato: ClassifiedArbitraryTypedObject): HTMLParagraphElement {
     const e = document.createElement('p');
-    e.innerText = cato.nodes[0]['text'];
+    const theNode = cato.nodes[0];
+
+    for (let c of theNode['children']) {
+      const marked = this.appendMarks(theNode, c);
+      if (marked instanceof HTMLElement) {
+        e.innerHTML += marked.outerHTML;
+      } else {
+        e.innerHTML += marked;
+      }
+    }
+
     return e;
+  }
+
+  hasMarks(child: any): boolean {
+    return child['marks'] && child['marks']['length'] > 0;
+  }
+
+  appendMarks(node: any, child: any): HTMLElement | string {
+    if (!this.hasMarks(child) || !node['markDefs'] || node['markDefs']['length'] < 1) {
+      return child.text;
+    }
+
+    let s = child.text;
+
+    for (let mark of child['marks']) {
+      for (let markDef of node['markDefs']) {
+        if (mark === markDef['_key']) {
+          let marker = this.markerProvider.getMarker(markDef['_type']);
+
+          if (!marker) {
+            console.warn('No marker found for markDef type ' + markDef['_type']);
+            continue;
+          }
+
+          s = marker?.applyMarks(s, {markDef});
+        }
+      }
+    }
+
+    return s;
   }
 
   /**
